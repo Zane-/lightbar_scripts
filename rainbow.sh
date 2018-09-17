d9fb9330d000c32e2151c7d9fcd698dd750329be
Zane Bilous <Zanebilous@gmail.com>
2018-09-17 19:13:10 -0400

    Update rainbow.sh

--- rainbow.sh
+++ rainbow.sh
@@ -7,7 +7,7 @@ sudo ectool lightbar 4 00 00 00
 
 echo "Press Ctrl + C to quit"
 
-while [ $run -eq 1 ]:
+while [ $run -eq 1 ];
 do
     sudo ectool lightbar 3 00 00 FF
     sudo ectool lightbar 2 00 EE 00
